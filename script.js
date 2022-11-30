748f0486cc98915b5f1ca2dcbd1d7e76e616c848
Jakub Antonin Stigler <jakubstig@gmail.com>
2022-11-30 16:39:08 +0100

    Add achievements

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.querySelector(".cookies").style.bottom = "0";
 
 function hideCookies() {
     document.querySelector(".cookies").style.bottom = "-5em";
+    showAchievement("hidCook");
 }
 
 function goUp() {
@@ -16,7 +17,7 @@ function goUp() {
 
 window.onscroll = () => {
     if (window.scrollY == 0)
-        showAchievment("conSight");
+        showAchievement("conSight");
     if (window.scrollY > document.getElementById("title").offsetTop)
         document.querySelector(".goUp").style.bottom = "3em";
     else
@@ -25,7 +26,7 @@ window.onscroll = () => {
 
 let unlocked = { "lol": false };
 
-function showAchievment(type) {
+function showAchievement(type) {
     if (unlocked[type])
         return;
     let name = "Name";
@@ -35,26 +36,41 @@ function showAchievment(type) {
     {
         case "conSight":
             name = "Construction sight";
-            msg = "See the CONSTRUCTION";
+            msg = "There is actually CONSTRUCTION up there";
+            break;
+        case "hidCook":
+            name = "The cookie is a lie!";
+            msg = "There is no cookie, but I guess you already knew that.";
+            break;
+        case "allLinks":
+            name = "Click on everything!";
+            msg = "Click on every link in contents";
             break;
     }
 
-    document.querySelector(".achievmentName").innerHTML = name;
-    document.querySelector(".achievmentMsg").innerHTML = msg;
+    document.querySelector(".achievementName").innerHTML = name;
+    document.querySelector(".achievementMsg").innerHTML = msg;
 
-    document.querySelector(".achievment").style.bottom = "0";
+    document.querySelector(".achievement").style.bottom = "0";
 
-    setTimeout(() => document.querySelector(".achievment").style.bottom = "-5em", 3000);
+    setTimeout(() => document.querySelector(".achievement").style.bottom = "-7em", 3000);
 }
 
 // set smooth scrolling on links
 // this cannot be done trough the html style scroll-behaviour
 // because the initial scroll must be instant
+let n = 1;
+let allLinks = 0;
 let anchorSelector = 'a[href^="#"]';
 let anchorList = document.querySelectorAll(anchorSelector);
 anchorList.forEach(link => {
-    link.onclick = (e) => {
+    let v = n;
+    n <<= 1;
+    link.onclick = function (e) {
         e.preventDefault();
+        allLinks |= v;
+        if (allLinks == 7)
+            showAchievement("allLinks");
         let destination = document.querySelector(this.hash);
         destination.scrollIntoView({
             behavior: 'smooth'
