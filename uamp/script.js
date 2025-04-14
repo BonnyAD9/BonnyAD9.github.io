7e306dc49e9133615b0f97007d4d0e3965081d2f
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2025-04-14 15:22:36 +0200

    Make the rng style general.

--- uamp/script.js
+++ uamp/script.js
@@ -1,13 +1,29 @@
 for (let e of document.getElementsByClassName("rng-letter")) {
-    let tex_len = 703;
-    let tex = "HELLO WORLD"
+    rng_text_hover(e);
+}
 
-    e.firstElementChild.innerHTML = rng_text(tex.length, tex.toLowerCase());
-    e.addEventListener("mousemove", () => {
-        e.firstElementChild.innerHTML = rng_text(tex_len, tex);
+/**
+ *
+ * @param {Element} elem
+ */
+function rng_text_hover(elem) {
+    let [cw, ch] = getCharSize(elem, "M");
+
+    let lines = Math.floor((elem.offsetHeight + 1) / ch);
+    lines -= (lines - 1) & 1; // There must be odd number of lines
+    let cols = Math.floor((elem.offsetWidth - 1) / cw);
+
+    let tex_len = lines * cols;
+    let text = elem.textContent.trim();
+
+    elem.innerHTML = '<div></div><div class="rng-letter-inner"></div>';
+    elem.firstElementChild.innerHTML = rng_text(text.length, text);
+    elem.addEventListener("mousemove", () => {
+        elem.firstElementChild.innerHTML =
+            rng_text(tex_len, text.toUpperCase());
     });
-    e.addEventListener("mouseleave", () => {
-        e.firstElementChild.innerHTML = rng_text(tex.length, tex.toLowerCase());
+    elem.addEventListener("mouseleave", () => {
+        elem.firstElementChild.innerHTML = rng_text(text.length, text);
     });
 }
 
@@ -25,7 +41,56 @@ function rng_text(len, s) {
         res += String.fromCharCode(Math.floor(Math.random() * chrs) + 33);
     }
     let i = res.length / 2;
-    return escape(res.slice(0, i)) + '<span class="rng-fg">' + s + '</span>' + escape(res.slice(i, res.length));
+    return escape(res.slice(0, i))
+        + '<span class="rng-fg">'
+        + s
+        + '</span>'
+        + escape(res.slice(i, res.length));
+}
+
+/**
+ *
+ * @param {Element} elem
+ * @param {string} chr
+ * @returns {float[]}
+ */
+function getCharSize(elem, chr) {
+    const REPW = 8;
+    const REPH = 8;
+    let short = "";
+    let long = chr;
+    for (let i = 0; i < REPW; ++i) {
+        short += chr;
+    }
+    for (let i = 1; i < REPH; ++i) {
+        long += '<br>' + chr;
+    }
+
+    let sty = document.defaultView.getComputedStyle(elem);
+
+    let text = document.createElement("div");
+    text.style.fontFamily = sty.fontFamily;
+    text.style.fontSize = sty.fontSize;
+    text.style.height = 'auto';
+    text.style.width = 'auto';
+    text.style.position = 'absolute';
+    text.style.visibility = 'hidden';
+    text.style.top = 0;
+
+    document.body.appendChild(text);
+
+    text.innerHTML = short;
+
+    let width = text.offsetWidth;
+
+    text.innerHTML = long;
+
+    let height = text.offsetHeight;
+
+    let cw = width / REPW;
+    let ch = height / REPH;
+
+    return [cw, ch];
 }
 
 function escape(s) {
